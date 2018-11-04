220e2aae1ce60ab97ef768ba4cdb1211283de59f
Caroline Salib <carolinesalibc@gmail.com>
2018-11-04 19:09:04 -0200

    Add poster component

---
+++ src/components/Poster.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Poster.css'
+
+const Poster = () => {
+  return (
+    <div className="Poster">
+      <span>Poster</span>
+      <img src='https://ae01.alicdn.com/kf/HTB1cppzSFXXXXcGXpXXq6xXFXXX6/-.jpg_640x640.jpg' alt='Green Day Poster' />
+      <button>Add</button>
+    </div>
+  )
+}
+
+export default Poster
