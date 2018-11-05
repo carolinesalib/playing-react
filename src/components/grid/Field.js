276d2b18e6ec0ad69cc389397002f6ca8a781a61
Caroline Salib <carolinesalibc@gmail.com>
2018-11-05 18:01:02 -0200

    Css improvements

--- src/components/grid/Field.js
+++ src/components/grid/Field.js
@@ -4,20 +4,18 @@ import './Field.css';
 const Field = ( props ) => {
   return (
     <div className="Field">
-      <form className="Field-form" onBlur={props.blur}>
-        <div className="Field-header">
-          <span className="Field-title"></span>
-        </div>
-        <input
-          className="Field-input"
-          type="text"
-          type="number"
-          onChange={props.change}
-          value={props.value}
-          style={{ color: props.color || 'grey' }}
-          readOnly={props.change ? false : true}
-        />
-      </form>
+      <div className="Field-header">
+        <span className="Field-title"></span>
+      </div>
+      <input
+        className="Field-input"
+        type="text"
+        type="number"
+        onChange={props.change}
+        value={props.value}
+        style={{ color: props.color || '#BEBEC1' }}
+        readOnly={props.change ? false : true}
+      />
     </div>
   );
 }
